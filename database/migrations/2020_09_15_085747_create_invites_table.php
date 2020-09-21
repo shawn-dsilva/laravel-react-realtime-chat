f7bc44c5a824fd15c39570c01b1e6b94b23be1c5
Shawn Dsilva <shawn@shawndsilva.com>
2020-09-21 17:20:17 +0530

    invites table model and migration done

--- database/migrations/2020_09_15_085747_create_invites_table.php
+++ database/migrations/2020_09_15_085747_create_invites_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateInviteTable extends Migration
+class CreateInvitesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateInviteTable extends Migration
      */
     public function up()
     {
-        Schema::create('invite', function (Blueprint $table) {
+        Schema::create('invites', function (Blueprint $table) {
             $table->id();
             $table->integer('from_id')->unsigned();
             $table->integer('to_id')->unsigned();
@@ -29,6 +29,6 @@ class CreateInviteTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('invite');
+        Schema::dropIfExists('invites');
     }
 }
