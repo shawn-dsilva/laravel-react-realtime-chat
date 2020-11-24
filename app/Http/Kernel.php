e59182b151cf152df43ab220fd680d823e060dc7
Shawn Dsilva <shawn@shawndsilva.com>
2020-11-24 23:16:16 +0530

    User online status middleware development start

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -37,6 +37,8 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \App\Http\Middleware\LastUserActivity::class,
+
         ],
 
         'api' => [
---
+++ app/Http/Middleware/LastUserActivity.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Cache;
+use Carbon\Carbon;
+
+class LastUserActivity
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if(Auth::check()) {
+            $expiresAt = Carbon::now()->addMinutes(1);
+            Cache::put('user-is-online-' . Auth::user()->id, true, $expiresAt);
+        }
+        return $next($request);
+    }
+}
