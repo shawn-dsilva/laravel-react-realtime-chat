f5d019a21b1f8674a23b918438d8e400bf2d039b
Shawn Dsilva <shawn@shawndsilva.com>
2020-12-01 23:17:47 +0530

    chat channels list css fixed

--- resources/js/components/ChatChannelsList.js
+++ resources/js/components/ChatChannelsList.js
@@ -1,42 +1,64 @@
-
-
 import React from "react";
 import { Col, Button } from "reactstrap";
-import CreateChannelModal from './CreateChannelModal';
+import CreateChannelModal from "./CreateChannelModal";
 
-
-export const ChatChannelsList = (props) => {
+export const ChatChannelsList = props => {
     const channels = props.channels;
     // console.log(typeof(channels));
-  const { channelSelect } = props;
-  console.log("in channels list");
-  console.log(channels);
+    const { channelSelect } = props;
+    console.log("in channels list");
+    console.log(channels);
     const channelList = channels.map((value, index) => {
         return (
-            <Col className="channelElement" key={index}>
-                <Button color="link"
-                    onClick={() => channelSelect(value.id, value.name, value.desc, value.owner_id, value.owner)}
-                    id={value.id}>
+            <div className="channelElement" key={index}>
+                <Button
+                    color="link"
+                    onClick={() =>
+                        channelSelect(
+                            value.id,
+                            value.name,
+                            value.desc,
+                            value.owner_id,
+                            value.owner
+                        )
+                    }
+                    id={value.id}
+                >
                     <b>{value.name}</b>
                 </Button>
                 <br></br>
-            </Col>
+            </div>
         );
     });
 
-
     return (
-      <div className="sidepaneChannel" >
-      <h3>Channels</h3>
-      <CreateChannelModal buttonLabel={"+ Create New Channel"}/>
-      <br></br>
-
-       <Col> <Button color="link" onClick={() => channelSelect(5, 'General', 'A public channel where all users can chat')} id="5" key="5"><b> General</b></Button>
-       <hr></hr>
-        {channelList}
-  </Col>
-  </div>
-    )
+        <div className="sidepaneParent">
+            <h3>Channels</h3>
+            <CreateChannelModal buttonLabel={"+ Create New Channel"} />
+            <br></br>
+            <div className="sidepaneChannel">
+                <div>
+                    {" "}
+                    <Button
+                        color="link"
+                        onClick={() =>
+                            channelSelect(
+                                5,
+                                "General",
+                                "A public channel where all users can chat"
+                            )
+                        }
+                        id="5"
+                        key="5"
+                    >
+                        <b> General</b>
+                    </Button>
+                    <hr></hr>
+                    {channelList}
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default ChatChannelsList;
