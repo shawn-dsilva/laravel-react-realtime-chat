c855d6731d28840cc45cfc9babba91c0e57e5ed6
Shawn Dsilva <shawn@shawndsilva.com>
2021-01-14 21:54:06 +0530

    baseURL added for axios requests

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -23,6 +23,8 @@ window.axios = require('axios');
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
+window.axios.defaults.baseURL = 'http://demos.shawndsilva.com/realtime-chat-app/';
+
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
  * for events that are broadcast by Laravel. Echo and event broadcasting
