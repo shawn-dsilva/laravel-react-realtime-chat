d20d129a2a4ab04a236418d8f52779a9a3152629
Shawn Dsilva <shawn@shawndsilva.com>
2021-02-20 22:08:10 +0530

    Image upload from React front end and store in directory functionality successfully implemented

--- app/Http/Controllers/ImageUploadController.php
+++ app/Http/Controllers/ImageUploadController.php
@@ -1,19 +1,27 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use Illuminate\Http\File;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Http\Request;
 
 class ImageUploadController extends Controller
 {
     public function updateProfilePicture(Request $request) {
-        $file = $request->file('profileImage');
-     
-        error_log('File Name: '.$file->getClientOriginalName());
-        error_log('File Extension: '.$file->getClientOriginalExtension());
-        error_log('File Real Path: '.$file->getRealPath());
-        error_log('File Size: '.$file->getSize());
-        error_log('File Mime Type: '.$file->getMimeType());
+        $image = $request->file('profileImage');
+
+        // Stores the Image into a folder called Avatars, 
+        // image name is generated and assigned to $path
+        // disk("public") is the local directory storage/app/public
+        $path = Storage::disk('public')->put('avatars', $image);
+
+
+        error_log('File Name: '.$image->getClientOriginalName());
+        error_log('File Extension: '.$image->getClientOriginalExtension());
+        error_log('File Real Path: '.$image->getRealPath());
+        error_log('File Size: '.$image->getSize());
+        error_log('File Mime Type: '.$image->getMimeType());
+        error_log($path);
 
      }
 }
