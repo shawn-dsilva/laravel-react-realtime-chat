5e19ca32185e0b5edb77badd30aee50c4a604ed4
Shawn Dsilva <shawn@shawndsilva.com>
2021-03-24 17:38:41 +0530

    CSS errors fixed

--- resources/js/components/HomeNav.js
+++ resources/js/components/HomeNav.js
@@ -37,10 +37,10 @@ const HomeNav = (props) => {
                 <NavLink href="https://www.github.com/shawn-dsilva/realtime-chat-app">Source on GitHub</NavLink>
               </NavItem>
               <NavItem>
-                <NavLink href="/login">Login</NavLink>
+                <NavLink href="login">Login</NavLink>
               </NavItem>
               <NavItem>
-                <NavLink href="/register">Register</NavLink>
+                <NavLink href="register">Register</NavLink>
               </NavItem>
             </Nav>
           </Collapse>
